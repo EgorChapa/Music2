264dcc9b8951bb09ca79bb0ddce4dc879e6c7a5e
Egor <Capl_iu11@student.itstep.org>
2022-02-17 21:53:41 +0200

    V 0.1

--- Music2/Form1.Designer.cs
+++ Music2/Form1.Designer.cs
@@ -1,4 +1,10 @@
 ﻿
+using NAudio.Wave;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
 namespace Music2
 {
     partial class Form1
@@ -31,10 +37,86 @@ namespace Music2
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Text = "Player";
+
+            delete_music.Text = "Delete";
+            delete_music.Location = new Point(300, 300);
+            delete_music.Size = new System.Drawing.Size(50, 50);
+            Controls.Add(delete_music);
+
+            play.Location = new Point(175, 150);
+            play.Size = new System.Drawing.Size(50, 50);
+            play.SizeMode = PictureBoxSizeMode.Zoom;
+            play.Image = Image.FromFile("2088539.png");
+            Controls.Add(play);
+
+            right.Location = new Point(225, 150);
+            right.Text = ">";
+            right.Size = new System.Drawing.Size(50, 50);
+            Controls.Add(right);
+
+            left.Location = new Point(125, 150);
+            left.Text = "<";
+            left.Size = new System.Drawing.Size(50, 50);
+            Controls.Add(left);
+
+            add_music.Location = new Point(300, 250);
+            add_music.Text = "Add";
+            add_music.Size = new System.Drawing.Size(50, 50);
+            add_music.Click += Add_music_Click;
+            Controls.Add(add_music);
+
+            image.Location = new Point(125, 0);
+            image.Size = new System.Drawing.Size(150, 150);
+            image.SizeMode = PictureBoxSizeMode.Zoom;
+            image.Image = Image.FromFile("CompactDisc.jpg");
+            Controls.Add(image);
+
+            Controls.Add(info);
+
+            musics.Size = new System.Drawing.Size(250, 200);
+            musics.Location = new Point(50, 250);
+            Controls.Add(musics);
+        }
+        public int music_ = 0;
+        bool x = true;
+        string[] path;
+        private void Add_music_Click(object sender, System.EventArgs e)
+        {
+            music_ = path.Length - 1;
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "*.mp3|*.mp3|*.wav|*.wav";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    music_++;
+                    paths.Add(dialog.FileName);
+                    musics.Items.Add(dialog.SafeFileName);
+
+                    //musica.Last().ToString()
+                    //musics.Items.Add(musica.Last().ToString()); надо
+                }
+            }
         }
 
+        Button add_music = new Button();
+        Button delete_music = new Button();
+        PictureBox play = new PictureBox();
+        Button right = new Button();
+        Button left = new Button();
+
+        Label info = new Label();
+
+        PictureBox image = new PictureBox();
+
+        List<string> paths = new List<string>();
+        ListBox musics = new ListBox();
+
+        AudioFileReader audioFile;
+        WaveOutEvent outputDevice;
+
+        FileInfo file_;
         #endregion
     }
 }
