4e01a78524fc4e51bc6900369d04fa24211f17c0
Egor <Capl_iu11@student.itstep.org>
2022-02-17 21:40:49 +0200

    Добавьте файлы проекта.

---
+++ Music2/Program.cs
@@ -0,0 +1,24 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Music2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
